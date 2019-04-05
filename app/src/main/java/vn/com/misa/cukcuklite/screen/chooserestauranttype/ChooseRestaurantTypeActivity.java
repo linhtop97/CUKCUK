d584740bc09d867525d40a63f1359e238e5e5c85
linhnb <nguyenbalinh06031997@gmail.com>
2019-04-05 10:45:19 +0700

    show list restaurent type complete

--- app/src/main/java/vn/com/misa/cukcuklite/screen/chooserestauranttype/ChooseRestaurantTypeActivity.java
+++ app/src/main/java/vn/com/misa/cukcuklite/screen/chooserestauranttype/ChooseRestaurantTypeActivity.java
@@ -1,11 +1,16 @@
 package vn.com.misa.cukcuklite.screen.chooserestauranttype;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,11 +18,20 @@ import vn.com.misa.cukcuklite.R;
 import vn.com.misa.cukcuklite.data.models.RestaurantType;
 import vn.com.misa.cukcuklite.utils.AppConstants;
 
-public class ChooseRestaurantTypeActivity extends AppCompatActivity implements IChooseRestaurantTypeContract.IView {
+/**
+ * Màn hình lựa chọn loại nhà hàng
+ * Created_by Nguyễn Bá Linh on 05/04/2019
+ */
+public class ChooseRestaurantTypeActivity extends AppCompatActivity implements IChooseRestaurantTypeContract.IView, View.OnClickListener {
 
     private ImageButton btnBack;
+    private TextView tvContinue;
     private boolean mIsLoginBefore;
     private ChooseRestaurantTypePresenter mPresenter;
+    private Button btnContinue;
+    private RecyclerView rvRestaurantType;
+    private RestaurantTypeAdapter mRestaurantTypeAdapter;
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,7 +50,13 @@ public class ChooseRestaurantTypeActivity extends AppCompatActivity implements I
      * Created_by Nguyễn Bá Linh on 03/04/2019
      */
     private void initEvents() {
-
+        try {
+            btnBack.setOnClickListener(this);
+            btnContinue.setOnClickListener(this);
+            tvContinue.setOnClickListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -44,19 +64,50 @@ public class ChooseRestaurantTypeActivity extends AppCompatActivity implements I
      * Created_by Nguyễn Bá Linh on 03/04/2019
      */
     private void initViews() {
-        btnBack = findViewById(R.id.btnBack);
-        Intent intent = getIntent();
-        mIsLoginBefore = intent.getBooleanExtra(AppConstants.EXTRA_LOGIN_SUCCESS, false);
-        if (mIsLoginBefore) {
-            btnBack.setVisibility(View.GONE);
-        }
+        try {
+            btnBack = findViewById(R.id.btnBack);
+            tvContinue = findViewById(R.id.tvContinue);
+            btnContinue = findViewById(R.id.btnContinue);
+            rvRestaurantType = findViewById(R.id.rvRestaurantType);
+            Intent intent = getIntent();
+            mIsLoginBefore = intent.getBooleanExtra(AppConstants.EXTRA_LOGIN_SUCCESS, false);
+            if (mIsLoginBefore) {
+                btnBack.setVisibility(View.GONE);
+            }
 
+            rvRestaurantType.setLayoutManager(new LinearLayoutManager(this));
+            mRestaurantTypeAdapter = new RestaurantTypeAdapter(this);
+            rvRestaurantType.setAdapter(mRestaurantTypeAdapter);
+            initProgressBar();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
+    /**
+     * Phương thức khởi tạo progressbar
+     * Created_by Nguyễn Bá Linh on 05/04/2019
+     */
+    private void initProgressBar() {
+        try {
+            mDialog = new ProgressDialog(this) {
+                @Override
+                public void onBackPressed() {
+                    super.onBackPressed();
+                }
+            };
+            mDialog.setMessage(getString(R.string.login_in_process));
+            mDialog.setCancelable(false);
+            mDialog.setCanceledOnTouchOutside(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void showListRestaurantType(List<RestaurantType> restaurantTypeList) {
-
+        mRestaurantTypeAdapter.setListData(restaurantTypeList);
     }
 
     @Override
@@ -70,7 +121,13 @@ public class ChooseRestaurantTypeActivity extends AppCompatActivity implements I
      */
     @Override
     public void showLoading() {
-
+        try {
+            if (mDialog != null && !mDialog.isShowing()) {
+                mDialog.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -79,6 +136,37 @@ public class ChooseRestaurantTypeActivity extends AppCompatActivity implements I
      */
     @Override
     public void hideLoading() {
+        try {
+            if (mDialog != null && mDialog.isShowing()) {
+                mDialog.dismiss();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    /**
+     * Phương thức xử lý các sự kiện click cho các view được gắn sự kiện OnClick
+     * Created_by Nguyễn Bá Linh on 05/04/2019
+     *
+     * @param v - view xảy ra sự kiện
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnBack:
+                finish();
+                break;
+            case R.id.btnContinue:
+            case R.id.tvContinue:
+                try {
+                    //bắt đầu main activity
+                    int restaurantTypeId = mRestaurantTypeAdapter.getRestaurantTypeIdSelected();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/vn/com/misa/cukcuklite/screen/chooserestauranttype/RestaurantTypeAdapter.java
@@ -0,0 +1,105 @@
+package vn.com.misa.cukcuklite.screen.chooserestauranttype;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import vn.com.misa.cukcuklite.R;
+import vn.com.misa.cukcuklite.base.adapters.ListAdapter;
+import vn.com.misa.cukcuklite.data.models.RestaurantType;
+
+public class RestaurantTypeAdapter extends ListAdapter<RestaurantType> {
+
+    private int mLastPositionSelected;
+
+    /**
+     * Là phương thức khởi tạo cho ListAdapter
+     * Created_by Nguyễn Bá Linh on 25/03/2019
+     *
+     * @param context là được truyền tới từ context nơi khởi tạo thể hiện của lớp
+     */
+    public RestaurantTypeAdapter(Context context) {
+        super(context);
+    }
+
+    public int getRestaurantTypeIdSelected() {
+        try {
+            return mListData.get(mLastPositionSelected).getRestaurantTypeId();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_restaurant_type, viewGroup, false);
+        return new RestaurantTypeHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        try {
+            RestaurantTypeHolder holder = (RestaurantTypeHolder) viewHolder;
+            holder.bind(mListData.get(i));
+            if (i == mLastPositionSelected) {
+                holder.ivSelected.setImageResource(R.drawable.ic_check_blue);
+            } else {
+                holder.ivSelected.setImageResource(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Lớp item cho loại nhà hàng/quán ăn
+     * Created_by Nguyễn Bá Linh on 05/04/2019
+     */
+    public class RestaurantTypeHolder extends RecyclerView.ViewHolder {
+        private TextView tvRestaurantType;
+        private ImageView ivSelected;
+
+        public RestaurantTypeHolder(@NonNull View itemView) {
+            super(itemView);
+            tvRestaurantType = itemView.findViewById(R.id.tvRestaurantType);
+            ivSelected = itemView.findViewById(R.id.ivSelected);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        int beforePosition = mLastPositionSelected;
+                        mLastPositionSelected = getAdapterPosition();
+                        notifyItemChanged(beforePosition);
+                        notifyItemChanged(mLastPositionSelected);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        /**
+         * phương thức gán dữ liệu cho item
+         * Created_by Nguyễn Bá Linh on 05/04/2019
+         *
+         * @param restaurantType - loại quán ăn/nhà hàng
+         */
+        void bind(RestaurantType restaurantType) {
+            try {
+                if (restaurantType == null) {
+                    return;
+                }
+                tvRestaurantType.setText(restaurantType.getRestaurantTypeName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
